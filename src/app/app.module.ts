734c63ca5dea9c025f53d5872814e3af2e3207cd
John Orji <john.orji@cognizant.com>
2021-01-22 15:39:58 -0600

    removed contact related features, updated displayname in yaml

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ContactCreateComponent } from './contact-create/contact-create.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
@@ -15,8 +13,6 @@ import { MenuComponent } from './rock-paper-scissors/menu/menu.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactCreateComponent,
-    ContactListComponent,
     HeaderComponent,
     FooterComponent,
     MenuComponent
