937495a0f43909f089e8bc0dc920844fdf94e73a
John Orji <john.orji@cognizant.com>
2021-01-25 16:43:31 -0600

    added tests for menu service, enhanced UI

--- src/app/services/menu-data.service.spec.ts
+++ src/app/services/menu-data.service.spec.ts
@@ -13,4 +13,26 @@ describe('MenuDataService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get and set menu data for opponent', () => {
+    var expected = '';
+
+    expect(service.data.opponent).toBe(expected);
+
+    expected = 'Dio';
+    service.data.opponent = expected;
+
+    expect(service.data.opponent).toBe(expected);
+  });
+
+  it('should get and set menu data for difficulty', () => {
+    var expected = 0;
+
+    expect(service.data.difficulty).toBe(expected);
+
+    expected = ++expected;
+    service.data.difficulty = expected;
+    
+    expect(service.data.difficulty).toBe(expected);
+  });
 });
